16f1a762c4dc80f65db5142bbf1fbb271216fe12
Kushagra Srivastava <kushagra2410@gmail.com>
2022-04-18 21:57:01 -0400

    Initial Prototyping
    Right now the big bang has happened and stars are forming :)

--- map.r
+++ map.r
@@ -1,6 +1,17 @@
 library(leaflet)
 library(tidyverse)
+library(sf)
+library(here)
+
+set.seed(69)
+randomNumbers <- sample.int(100, 138, replace = TRUE)
+
+root1 <- here("data", "mbta_rapid_transit", "MBTA_ARC.shp") %>%
+  st_read() %>%
+  st_transform(4326) %>% # Weird coordinate system: dont question
+  mutate(score = randomNumbers)
 
 # Leaflet function from leaflet package which makes a leaflet map
 leaflet() %>%
-  addTiles()
+  addTiles() %>%
+  addPolylines(data = root1, label = root1$score)
